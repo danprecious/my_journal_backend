d0df11150e17fe70c8a48a44a938ea88546b60dd
Kayode Dan Precious <danielkayode222@gmail.com>
2025-02-03 09:37:39 +0100

    implementing resetPassword

--- src/controllers/userController/forgotPasswordController.ts
+++ src/controllers/userController/forgotPasswordController.ts
@@ -1,18 +1,23 @@
 import { Request, Response } from "express";
 import { findUser } from "services/userServices.js";
-import crypto from 'crypto';
+import crypto from "crypto";
+import nodemailer from "nodemailer";
 
+const generateToken = () => {
+  return crypto.randomBytes(20).toString("hex");
+};
 
-
-const generateToken = () =>  {
-    return crypto.randomBytes(20).toString("hex");
-}
-
-
-const sendResetPasswordMail = (email: string, token:string) => {
-
-}
-
+const sendResetPasswordMail = (email: string, token: string) => {
+  const transporter = nodemailer.createTransport({
+    host: "",
+    port: 587,
+    secure: false, // true for port 465, false for other ports
+    auth: {
+      user: "maddison53@ethereal.email",
+      pass: "jn7jnAPss4f63QBp6D",
+    },
+  });
+};
 
 export const forgotPaswordController = async (
   req: Request<{}, {}, { email: string }>,
@@ -25,8 +30,8 @@ export const forgotPaswordController = async (
 
     const userExists = await findUser(email);
 
-    if (userExists) {
-// const token = 
+    if (!userExists) {
+      res.status(401).json({ error: "This user doesn't exist" });
     }
   } catch (error) {
     console.log("Internal server error");
