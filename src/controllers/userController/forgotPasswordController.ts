d5d931f8fb63b143454f4ffb11d35c1f626e55ce
Kayode Dan Precious <danielkayode222@gmail.com>
2025-02-03 11:12:03 +0100

    implementing resetPassword

--- src/controllers/userController/forgotPasswordController.ts
+++ src/controllers/userController/forgotPasswordController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { findUser } from "services/userServices.js";
 import crypto from "crypto";
 import nodemailer from "nodemailer";
+import prisma from "prisma/client.js";
 
 const generateToken = () => {
   return crypto.randomBytes(20).toString("hex");
@@ -17,6 +18,16 @@ const sendResetPasswordMail = (email: string, token: string) => {
       pass: "jn7jnAPss4f63QBp6D",
     },
   });
+
+  const mailOptions = {
+    to: email,
+    subject: "Reset Password",
+    text: `Click on the link to reset your password: https://myJournal.vercel.app/resetPassword?token=${token}`,
+  };
+
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) console.error("Error sending mail", error);
+  });
 };
 
 export const forgotPaswordController = async (
@@ -28,10 +39,20 @@ export const forgotPaswordController = async (
 
     console.log(email);
 
-    const userExists = await findUser(email);
+    const user = await findUser(email);
 
-    if (!userExists) {
+    if (!user) {
       res.status(401).json({ error: "This user doesn't exist" });
+    } else {
+      const token = generateToken();
+      const expiresAt = new Date(Date.now() + 3600 * 1000);
+      const resetToken = await prisma.resetToken.create({
+        data: {
+          userId: user.id,
+          token,
+          expiresAt: expiresAt,
+        },
+      });
     }
   } catch (error) {
     console.log("Internal server error");
